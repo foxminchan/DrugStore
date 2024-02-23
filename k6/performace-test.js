347a4230587b392b0125a7bd01fd38018edebf75
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2024-02-23 19:45:45 +0700

    test: update k6 script

--- k6/performace-test.js
+++ k6/performace-test.js
@@ -1,16 +1,29 @@
 import http from "k6/http";
 import { sleep } from "k6";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
-const BASE_URL = "http://localhost:8080/api/v1/Products";
+const BASE_URL = "https://localhost:8080/api/v1/Products";
 
-export let options = {
+export const options = {
   stages: [
     { duration: "30s", target: 20 },
     { duration: "1m", target: 20 },
     { duration: "30s", target: 0 },
     { duration: "30s", target: 60 },
   ],
+  thresholds: {
+    http_req_duration: ["p(95)<500"],
+    iterations: ["rate>100"],
+  },
 };
+
+export function handleSummary(data) {
+  return {
+    "result.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+  };
+}
 
 export default function () {
   http.get(BASE_URL);
