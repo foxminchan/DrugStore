3af57fe19d504c048e487c1ba86d8abd05259ee3
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2024-03-17 00:51:42 +0700

    ci: adjust nuke

--- src/DrugStore.Build/Build.cs
+++ src/DrugStore.Build/Build.cs
@@ -2,11 +2,6 @@ using Nuke.Common;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    /// - JetBrains ReSharper        https://nuke.build/resharper
-    /// - JetBrains Rider            https://nuke.build/rider
-    /// - Microsoft VisualStudio     https://nuke.build/visualstudio
-    /// - Microsoft VSCode           https://nuke.build/vscode
     public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
